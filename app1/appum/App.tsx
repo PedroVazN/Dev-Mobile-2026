ed38a84cda26ece3896f823cec080e05b508db2d
Pedro Vaz <vaznascimento23@gmail.com>
2026-02-27 18:52:51 -0300

    Upd01

---
+++ app1/appum/App.tsx
@@ -0,0 +1,45 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, ScrollView, Image,  } from 'react-native';
+
+export default function App() {
+  return (
+    <ScrollView style={styles.container}>
+      <Image source={require('./assets/bola.png')} style={styles.bola } />
+      <Text style={styles.text}>Recuperamo a bola.</Text>
+      <Text style={styles.textBola}>A bola havia sido perdida
+        porem recuperamos a bola com excelencia, {'\n'} {'\n'}
+        e agora ela esta conosco novamente.
+      </Text>
+      <StatusBar style="auto" />
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    marginTop: 60,
+
+  },
+  text: {
+    fontFamily: 'Arial', 
+    fontSize: 40,
+    color: '#fff',
+    paddingHorizontal: 20,
+  },
+  textBola: {
+    fontFamily: 'Arial', 
+    textAlign: 'center',
+    fontSize: 20,
+    marginTop: 20,
+    color: '#fff',
+    paddingHorizontal: 20,
+    textAlignVertical: 'center',
+  },
+  bola:{
+    width: 200, 
+    height: 200,
+    alignSelf: 'center',
+  }
+});
